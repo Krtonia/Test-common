8ca587f15042a1efc7aa7cb1cf057c216bcb07d2
Omkar Chandorkar <gotenksIN@aospa.co>
2023-05-11 20:37:00 +0000

    sdm845-common: uprev qti bluetooth_audio to 2.1
    Change-Id: I860a416bfe8da099ea65976e0bdcb5a9905d5ab0

--- sdm845.mk
+++ sdm845.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
