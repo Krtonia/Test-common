a89115a958fa1b13d3049d23f40f9dc935330f14
SonicBSV <sonicbsv@gmail.com>
2024-05-25 19:25:27 +0300

    sdm845-common: Add android.hardware.bluetooth@1.1

--- sdm845.mk
+++ sdm845.mk
@@ -98,6 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
